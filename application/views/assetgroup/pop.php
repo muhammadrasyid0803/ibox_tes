ffcde76c2008d0722c6a5876872e69347d5d98c7
ramanovaris <ramaanovariss@gmail.com>
2019-12-18 18:59:04 +0800

    Tampil Modal di Asset Group
    buka modal dgn ahref

--- application/views/assetgroup/pop.php
+++ application/views/assetgroup/pop.php
@@ -12,28 +12,28 @@
                 </div>
                 <div class="col-md-4">
                   <h5 class="gen-case">
-                     <a href="#" data-toggle="modal" data-target="modal_building">Building and Infrastructure</a>
+                     <a data-target="#modal_building" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_building">Building and Infrastructure</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">AC Electricity</a>
+                     <a data-target="#modal_ac" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_ac">AC Electricity</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">DC Electricity</a>
+                     <a data-target="#modal_dc" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_dc">DC Electricity</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">Power Supply Back Up System</a>
+                     <a data-target="#modal_power" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_power">Power Supply Back Up System</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">Cooling System</a>
+                     <a data-target="#modal_cooling" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_cooling">Cooling System</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">External Alarm And Monitoring System</a>
+                     <a data-target="#modal_alarm" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_alarm">External Alarm And Monitoring System</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">Network Device</a>
+                     <a data-target="#modal_network" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_network">Network Device</a>
                   </h5>
                   <h5 class="gen-case">
-                     <a href="">Passive Device (ODF)</a>
+                     <a data-target="#modal_device" data-toggle="modal" class="MainNavText" id="MainNavHelp" href="#modal_device">Passive Device (ODF)</a>
                   </h5>
                 </div>
                 </form>
@@ -43,13 +43,229 @@
           </div>
         </div>
 
-        <!-- Modal -->
+        <!-- Modal modal_building -->
               <div class="modal fade" id="modal_building" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
                       <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                      <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                      <h4 class="modal-title" id="myModalLabel">Building and Infrastructure</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_ac -->
+              <div class="modal fade" id="modal_ac" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">AC Electricity</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_dc -->
+              <div class="modal fade" id="modal_dc" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">DC Electricity</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_power -->
+              <div class="modal fade" id="modal_power" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">Power Supply Back Up System</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_cooling -->
+              <div class="modal fade" id="modal_cooling" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">Cooling System</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_network -->
+              <div class="modal fade" id="modal_network" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">Network Device</h4>
+                    </div>
+                    <div class="modal-body" id="detail_jobs">
+                      <label>Job Number :</label> <label class="job_number"></label>
+                      <br/>
+                      <label>Job Kategori :</label> <label class="job_kategori"></label>
+                      <br/>
+                      <label>Job SubKategori :</label> <label class="job_subkategori"></label>
+                      <br/>
+                      <label>Job Description :</label> <label class="job_desc"></label>
+                      <br/>
+                      <label>Lattitude :</label> <label class="lat"></label>
+                      <br/>
+                      <label>Longitude :</label> <label class="long"></label>
+                      <br/>
+                      <label>Saverity :</label> <label class="saverity"></label>
+                      <br/>
+                      <label>Priority :</label> <label class="priority"></label>
+                      <br/>
+                      <label>Date :</label> <label class="date_job"></label>
+                      <br/>
+                      <label>Impact (Berdampak) :</label> <label class="impact"></label>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+        <!-- Modal modal_device -->
+              <div class="modal fade" id="modal_device" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title" id="myModalLabel">Passive Device (ODF)</h4>
                     </div>
                     <div class="modal-body" id="detail_jobs">
                       <label>Job Number :</label> <label class="job_number"></label>
