42e56d3e2fdf5b87c6f48f627fabc4b2d9a2443c
ramanovaris <ramaanovariss@gmail.com>
2019-12-21 22:45:58 +0800

    Tambah button cetak & ambil di Job list
    Tapi masih belum berfungsi

--- application/views/jobs/index.php
+++ application/views/jobs/index.php
@@ -1,73 +1,4 @@
 <div class="row mt">
-          <!-- <div class="col-sm-3">
-            <section class="panel">
-              <div class="panel-body">
-                <a href="mail_compose.html" class="btn btn-compose">
-                  <i class="fa fa-pencil"></i>  Compose Mail
-                  </a>
-                <ul class="nav nav-pills nav-stacked mail-nav">
-                  <li class="active"><a href="inbox.html"> <i class="fa fa-inbox"></i> Inbox  <span class="label label-theme pull-right inbox-notification">3</span></a></li>
-                  <li><a href="#"> <i class="fa fa-envelope-o"></i> Send Mail</a></li>
-                  <li><a href="#"> <i class="fa fa-exclamation-circle"></i> Important</a></li>
-                  <li><a href="#"> <i class="fa fa-file-text-o"></i> Drafts <span class="label label-info pull-right inbox-notification">8</span></a></a>
-                  </li>
-                  <li><a href="#"> <i class="fa fa-trash-o"></i> Trash</a></li>
-                </ul>
-              </div>
-            </section>
-            <section class="panel">
-              <div class="panel-body">
-                <ul class="nav nav-pills nav-stacked labels-info ">
-                  <li>
-                    <h4>Friends Online</h4>
-                  </li>
-                  <li>
-                    <a href="#">
-                        <img src="img/friends/fr-10.jpg" class="img-circle" width="20">Laura
-                        <p><span class="label label-success">Available</span></p>
-                      </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                        <img src="img/friends/fr-05.jpg" class="img-circle" width="20">David
-                        <p><span class="label label-danger"> Busy</span></p>
-                      </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                        <img src="img/friends/fr-01.jpg" class="img-circle" width="20">Mark
-                        <p>Offline</p>
-                      </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                        <img src="img/friends/fr-03.jpg" class="img-circle" width="20">Phillip
-                        <p>Offline</p>
-                      </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                        <img src="img/friends/fr-02.jpg" class="img-circle" width="20">Joshua
-                        <p>Offline</p>
-                      </a>
-                  </li>
-                </ul>
-                <a href="#"> + Add More</a>
-                <div class="inbox-body text-center inbox-action">
-                  <div class="btn-group">
-                    <a class="btn mini btn-default" href="javascript:;">
-                      <i class="fa fa-power-off"></i>
-                      </a>
-                  </div>
-                  <div class="btn-group">
-                    <a class="btn mini btn-default" href="javascript:;">
-                      <i class="fa fa-cog"></i>
-                      </a>
-                  </div>
-                </div>
-              </div>
-            </section>
-          </div> -->
           <div class="col-sm-12">
             <section class="panel">
               <header class="panel-heading wht-bg">
@@ -82,51 +13,6 @@
               </header>
               <div class="panel-body minimal">
                 <div class="mail-option">
-                  <!-- <div class="chk-all">
-                    <div class="pull-left mail-checkbox">
-                      <input type="checkbox" class="">
-                    </div>
-                    <div class="btn-group">
-                      <a data-toggle="dropdown" href="#" class="btn mini all">
-                        All
-                        <i class="fa fa-angle-down "></i>
-                        </a>
-                      <ul class="dropdown-menu">
-                        <li><a href="#"> None</a></li>
-                        <li><a href="#"> Read</a></li>
-                        <li><a href="#"> Unread</a></li>
-                      </ul>
-                    </div>
-                  </div> -->
-                  <!-- <div class="btn-group">
-                    <a data-original-title="Refresh" data-placement="top" data-toggle="dropdown" href="#" class="btn mini tooltips">
-                      <i class=" fa fa-refresh"></i>
-                      </a>
-                  </div> -->
-                  <!-- <div class="btn-group hidden-phone">
-                    <a data-toggle="dropdown" href="#" class="btn mini blue">
-                      More
-                      <i class="fa fa-angle-down "></i>
-                      </a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#"><i class="fa fa-pencil"></i> Mark as Read</a></li>
-                      <li><a href="#"><i class="fa fa-ban"></i> Spam</a></li>
-                      <li class="divider"></li>
-                      <li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
-                    </ul>
-                  </div> -->
-                  <!-- <div class="btn-group">
-                    <a data-toggle="dropdown" href="#" class="btn mini blue">
-                      Move to
-                      <i class="fa fa-angle-down "></i>
-                      </a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#"><i class="fa fa-pencil"></i> Mark as Read</a></li>
-                      <li><a href="#"><i class="fa fa-ban"></i> Spam</a></li>
-                      <li class="divider"></li>
-                      <li><a href="#"><i class="fa fa-trash-o"></i> Delete</a></li>
-                    </ul>
-                  </div> -->
                   <ul class="unstyled inbox-pagination">
                     <li><span>1-50 of 99</span></li>
                     <li>
@@ -139,34 +25,8 @@
                 </div>
                 <div class="table-inbox-wrap ">
                   <table class="table table-inbox table-hover">
-                   <!--  <thead>
-                      <tr>
-                          <th>Job Number</th>
-                          <th>Description</th>
-                          <th>Location</th>
-                      </tr>
-                    </thead> -->
                     <tbody id="showdata">
-                     <!--  <tr class="unread">
-                        <td class="inbox-small-cells">
-                          <input type="checkbox" class="mail-checkbox">
-                        </td>
-                        <td class="inbox-small-cells"><i class="fa fa-star"></i></td>
-                        <td class="view-message  dont-show"><a href="mail_view.html">Google Inc.</a></td>
-                        <td class="view-message "><a href="mail_view.html">Your new account is ready.</a></td>
-                        <td class="view-message  inbox-small-cells"><i class="fa fa-paperclip"></i></td>
-                        <td class="view-message  text-right">08:10 AM</td>
-                      </tr>
-                      <tr class="unread">
-                        <td class="inbox-small-cells">
-                          <input type="checkbox" class="mail-checkbox">
-                        </td>
-                        <td class="inbox-small-cells"><i class="fa fa-star"></i></td>
-                        <td class="view-message  dont-show"><a href="mail_view.html">Google Inc.</a></td>
-                        <td class="view-message "><a href="mail_view.html">Your new account is ready.</a></td>
-                        <td class="view-message  inbox-small-cells"><i class="fa fa-paperclip"></i></td>
-                        <td class="view-message  text-right">08:10 AM</td>
-                      </tr> -->
+                     
                     </tbody>
                   </table>
                 </div>
@@ -175,12 +35,7 @@
           </div>
         </div>
 
-        <!--  MODALS -->
-              <!-- <h4><i class="fa fa-angle-right"></i> Modal Example</h4> -->
-              <!-- Button trigger modal -->
-             <!--  <button class="btn btn-success btn-lg" data-toggle="modal" data-target="#myModal">
-                Launch Modal
-                </button> -->
+   
               <!-- Modal -->
               <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                 <div class="modal-dialog">
@@ -236,7 +91,8 @@
                       '<td>'+data[i].description+'</td>'+
                        '<td>'+'<span class="label label-danger">Belum Selesai</span>'+'</td>'+
                       '<td>'+
-                        '<button class="btn btn-theme03 item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-ambil" data="'+data[i].id+'" style="background: #cc3300;">Ambil</button>'+
                       '</td>'+
                       '</tr>';
               }
@@ -245,7 +101,8 @@
                       '<td>'+data[i].description+'</td>'+
                        '<td>'+'<span class="label label-success">Selesai</span>'+'</td>'+
                       '<td>'+
-                        '<button class="btn btn-theme03 item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-cetak" data="'+data[i].id+'" style="background: #cc0099;">Cetak</button>'+
                       '</td>'+
                       '</tr>';  
               }
@@ -254,7 +111,8 @@
                       '<td>'+data[i].description+'</td>'+
                        '<td>'+'<span class="label label-warning">On Progress</span>'+'</td>'+
                       '<td>'+
-                        '<button class="btn btn-theme03 item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-tampil" data="'+data[i].id+'" style="background: #3f60f3;">Detail</button>'+
+                        '<button class="btn btn-theme03 btn-xs item-cetak" data="'+data[i].id+'" style="background: #cc0099;">Cetak</button>'+
                       '</td>'+
                       '</tr>';  
               }
